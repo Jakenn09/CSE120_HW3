5911dc2b7f313ba83eb9022f898f60f5a9b4f772
Jakenn09 <88737390+Jakenn09@users.noreply.github.com>
2022-07-14 18:59:29 -0400

    start to SpaceWeight2 code

--- SpaceWeight.py
+++ SpaceWeight.py
@@ -1,1 +1,37 @@
 # SpaceWeight2.py
+
+class Planet:
+    
+    Mercury = 0.38
+    Venus = 0.91
+    Mars = 0.38
+    Jupiter = 2.34
+    Saturn = 0.93
+    Uranus = 0.92
+    Neptune = 1.12
+    Pluto = 0.62
+
+    def calculate_spaceweight(self, weight, planet_diff):
+        planet_weight = weight * planet_diff
+        return planet_weight
+
+
+planet1 = Planet()
+weight = float(input("Please enter your weight: "))
+planet_diff = planet1.Mercury
+list1 = ["Mercury", "Venus", "Mars", "Jupiter", "Saturn", "Uransus", "Neptune", "Pluto"]
+print(planet1.Mars)
+
+#for i in range(len(list1)):
+    #print("Your weight on", list1[i], "is: ", planet1.calculate_spaceweight(weight, planet_diff))
+
+'''
+
+planet_diff = float(input("Please enter the planet diff: "))
+planet1.calculate_spaceweight(weight, planet_diff)
+
+
+
+print(weight * planet1.Mercury)
+print(planet1.Planet)
+'''
