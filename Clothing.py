8df51b6ff3e06f26506ed6f9564dda7e0b57372e
Jakenn09 <88737390+Jakenn09@users.noreply.github.com>
2022-07-14 21:52:10 -0400

    working clothing.py file

--- Clothing.py
+++ Clothing.py
@@ -1,1 +1,33 @@
 # Clothing.py
+
+class clothing:
+    def __init__(self, name, minTemp, maxTemp, formal):
+        self.name = name
+        self.minTemp = minTemp
+        self.maxTemp = maxTemp
+        self.formal = formal
+
+num_clothes = int(input("Please enter the number of clothing pieces you want to catalog: "))
+
+clothes_list = []
+
+for i in range(num_clothes):
+    name = input("Please enter the clothing item: ")
+    minTemp = input("Please enter the minimum temp you would like to wear the item: ")
+    maxTemp = input("Please enter the maximum temp you would like to wear the item: ")
+    formal = input("Would you wear this to a formal event? ")
+
+    clothes_list.append(clothing(name, minTemp, maxTemp, formal))
+
+temperature = input("Please enter the current temperature: ")
+is_formal = input("Is the item formal? ")
+
+print("Items that can be worn: ")
+
+for item in clothes_list:
+    if temperature >= item.minTemp and temperature <= item.maxTemp:
+        if is_formal.lower() == item.formal.lower():
+            print(item.name)
+
+
+
